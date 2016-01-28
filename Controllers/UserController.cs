074243df0c18dba495b03c5af355838ffa591189
nitinv-click2cloud <nitin.vaswani@click2cloud.net>
2016-01-28 14:53:31 +0530

    adding files for webApi

---
+++ Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using WebApi2.Model;
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new User[] { new User { Id = 1, UserName = "ABC", Password = "ABC" } };
+        }
+
+        // GET api/User/5
+       [HttpGet("{id}")]
+        //[ActionName("GetEmployeeByID")]
+        public User Get(int id)
+        {
+            //    //return listEmp.First(e => e.ID == id);  
+            SqlDataReader reader = null;
+             SqlConnection myConnection = new SqlConnection();
+            //myConnection.ConnectionString = @"Server=WSD001\SQLEXPRESS;Database=Nitin;User ID=rsinngp/nitin.vaswani;Password=Newuser@123;";
+            myConnection.ConnectionString = @"Data Source = WSD001\SQLEXPRESS;Initial Catalog = Nitin;Integrated Security = True";
+            SqlCommand sqlCmd = new SqlCommand();
+            //sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from [User] where Id=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            User emp = null;
+            while (reader.Read())
+            {
+                emp = new User();
+                emp.Id = Int32.Parse(reader.GetValue(0).ToString());
+                emp.UserName = reader.GetValue(1).ToString();
+                emp.Password = reader.GetValue(2).ToString();
+            }
+            myConnection.Close();
+            return emp;
+          
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApi2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5973330d-ffc6-4b3c-9848-6a27a5e100bc")]
